a00f867c5a3da9a2ee3e85de02ff22879625c8ce
Benny Jane <3355817143@qq.com>
2020-11-16 08:19:27 +0800

    a

---
+++ 多进程/demon_process.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/15
+# @Author : Benny Jane
+# @Email : 暂无
+# @File : 守护进程.py
+# @Project : Python-Exercise
+
+"""
+实现守护进程
+https://www.jianshu.com/p/7fd341a4251d
+"""
+
